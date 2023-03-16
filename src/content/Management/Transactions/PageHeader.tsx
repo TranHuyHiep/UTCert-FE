8021bc5e4228db42358df26e4add92e6e1caf244
TranHuyHiep <tranhuyhiep0710@gmail.com>
2023-03-17 02:11:58 +0700

    update up file csv

--- src/content/Management/Transactions/PageHeader.tsx
+++ src/content/Management/Transactions/PageHeader.tsx
@@ -1,31 +1,75 @@
-import { Typography, Button, Grid, Dialog, DialogTitle, List, ListItem, ListItemAvatar, Avatar, ListItemText } from '@mui/material';
+
+import {
+  Typography,
+  Button,
+  Grid,
+  Dialog,
+  DialogTitle,
+  List,
+  ListItem,
+  ListItemAvatar,
+  Avatar,
+  ListItemText
+} from '@mui/material';
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 import AddTwoToneIcon from '@mui/icons-material/AddTwoTone';
 import PersonIcon from '@mui/icons-material/Person';
 import { blue } from '@mui/material/colors';
 import AddIcon from '@mui/icons-material/Add';
-
+import GetCookie from '@/hooks/getCookie';
 
 function SimpleDialog(props) {
   const { onClose, selectedValue, open } = props;
+  const [selectedFile, setSelectedFile] = useState(null);
 
   const handleClose = () => {
     onClose(selectedValue);
   };
 
-  const handleListItemClick = (value) => {
-    onClose(value);
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+    handleListItemClick('addAccount');
+  };
+  
+  const handleListItemClick = async (destination) => {
+    if (destination === 'addAccount') {
+      if (selectedFile) {
+        const formData = new FormData();
+        formData.append('file', selectedFile);
+        formData.append('policyId', GetCookie('policyId'));
+
+        try {
+          const response = await fetch(
+            'https://localhost:44325/api/v1/Certificates/issuer',
+            {
+              method: 'POST',
+              body: formData
+            }
+          );
+
+          if (response.ok) {
+            console.log('File uploaded successfully');
+          } else {
+            console.error('Error uploading file');
+          }
+        } catch (error) {
+          console.error('Error uploading file', error);
+        }
+      } else {
+        console.error('No file selected');
+      }
+    }
   };
 
   return (
-    <Dialog onClose={handleClose} open={open}>
+    <Dialog open={open} onClose={handleClose}>
       <DialogTitle>Add file CSV</DialogTitle>
       <List sx={{ pt: 0 }}>
         <ListItem
           autoFocus
           button
-          onClick={() => handleListItemClick('addAccount')}
+          onClick={() => document.getElementById('fileInput').click()}
         >
           <ListItemAvatar>
             <Avatar>
@@ -33,6 +77,13 @@ function SimpleDialog(props) {
             </Avatar>
           </ListItemAvatar>
           <ListItemText primary=".csv" />
+          <input
+            type="file"
+            id="fileInput"
+            accept=".csv"
+            style={{ display: 'none' }}
+            onChange={handleFileChange}
+          />
         </ListItem>
       </List>
     </Dialog>
@@ -44,7 +95,6 @@ SimpleDialog.propTypes = {
   open: PropTypes.bool.isRequired,
   selectedValue: PropTypes.string.isRequired
 };
-
 
 function PageHeader() {
   const [open, setOpen] = useState(false);
@@ -80,10 +130,10 @@ function PageHeader() {
         >
           Create certificate
         </Button>
-        <SimpleDialog
-                  open={open}
-                  onClose={handleClose}
-                />
+        <SimpleDialog 
+          open={open} 
+          onClose={handleClose} 
+        />
       </Grid>
     </Grid>
   );
