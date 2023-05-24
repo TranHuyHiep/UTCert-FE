d94cfa82f21d784600f6d9f0352f985f1ff41f82
TranHuyHiep <tranhuyhiep0710@gmail.com>
2023-05-24 23:48:54 +0700

    done alert search

--- src/content/Overview/Search/index.tsx
+++ src/content/Overview/Search/index.tsx
@@ -1,6 +1,6 @@
 import { Button, Container, Dialog, DialogContent, Grid, Input } from "@mui/material";
 import { useState } from "react";
-import { SnackbarProvider, VariantType, useSnackbar } from 'notistack';
+import { SnackbarProvider, enqueueSnackbar, useSnackbar } from 'notistack';
 import React from "react";
 
 function hexToText(hexString) {
@@ -27,11 +27,11 @@ function SimpleDialog(props) {
         setCurrentIndex((currentIndex + 1) % certificates.length);
     };
 
-    if (!certificates || certificates.length === 0) {
+    if (!certificates || certificates.length === 0 || certificates.status_code == 404) {
         return null;
     }
 
-    console.log(certificates.onchain_metadata);
+    console.log(certificates);
 
     return (
         <Dialog open={open} onClose={onClose} maxWidth='lg'>
@@ -58,58 +58,47 @@ function SimpleDialog(props) {
         </Dialog>
     );
 }
-
-function MyApp() {
-    const { enqueueSnackbar } = useSnackbar();
-  
-    const handleClick = () => {
-      enqueueSnackbar('Not found certificates.', { variant: "error"});
-    };
-  
-    const handleClickVariant = (variant: VariantType) => () => {
-      // variant could be success, error, warning, info, or default
-      enqueueSnackbar('This is a success message!', { variant });
-    };
-  
-    return (
-      <React.Fragment>
-        <Button onClick={handleClick}>Show snackbar</Button>
-        <Button onClick={handleClickVariant('success')}>Show success snackbar</Button>
-      </React.Fragment>
-    );
-  }
-
 function Search() {
-    const [open, setOpen] = useState<boolean>(false);
-    const [inputValue, setInputValue] = useState("");
-    const [certificates, setCertificates] = useState("");
+    return (
+        <SnackbarProvider maxSnack={5}>
+            <App />
+        </SnackbarProvider>
+    );
+}
+
+const App = () => {
+    const [open, setOpen] = useState(false);
+    const [inputValue, setInputValue] = useState('');
+    const [certificates, setCertificates] = useState('');
+
+    const { enqueueSnackbar } = useSnackbar();
 
     const handleInputChange = (event) => {
         setInputValue(event.target.value);
     };
 
     const handleClickOpen = () => {
-        const url = "https://cardano-preprod.blockfrost.io/api/v0/assets/" + inputValue;
-        var projectId = "preproddZ8hPQ8b90t4TcBfnnnx7CPIJ4omEG1H";
+        const url =
+            'https://cardano-preprod.blockfrost.io/api/v0/assets/' + inputValue;
+        const projectId = 'preproddZ8hPQ8b90t4TcBfnnnx7CPIJ4omEG1H';
 
         fetch(url, {
             headers: {
-                "project_id": projectId
-            }
+                project_id: projectId,
+            },
         })
-            .then(response => response.json())
-            .then(data => {
-                // Xử lý dữ liệu trả về từ API\
-                if (data.status_code == 400) {
-                    alert("Not found")
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.status_code === 400 || data.status_code === 404) {
+                    enqueueSnackbar('Not found', { variant: 'error' });
                 } else {
-                    setCertificates(data)
+                    setCertificates(data);
                     setOpen(true);
                 }
             })
-            .catch(error => {
-                // Xử lý lỗi nếu có
-                console.error("khong tim thay");
+            .catch((error) => {
+                console.error('Error:', error);
+                enqueueSnackbar('An error occurred', { variant: 'error' });
             });
     };
 
@@ -117,39 +106,57 @@ function Search() {
         setOpen(false);
     };
     return (
-        <Container
-            maxWidth='lg'
-            sx={{
-                textAlign: 'center',
-            }}
-        >
-            <h1 style={{ color: 'white' }}>YOU CAN FIND CERTS IN HERE!</h1>
-            <Grid
-                justifyContent="center"
-                alignItems="center"
-                container
-                style={{ background: 'rgba(255, 255, 255, 0.2)', padding: '30px', marginTop: '10px', width: '800px', margin: '0 auto' }}
-            >
-                <Input value={inputValue}
-                    onChange={handleInputChange}
-                    placeholder="Certificate URL"
-                    type="text"
-                    style={{ width: '550px', fontSize: '20px', backgroundColor: 'white', height: '40px', paddingLeft: '10px' }}
+      <div>
+        <Container maxWidth="lg" sx={{ textAlign: 'center' }}>
+                <h1 style={{ color: 'white' }}>YOU CAN FIND CERTS IN HERE!</h1>
+                <Grid
+                    justifyContent="center"
+                    alignItems="center"
+                    container
+                    style={{
+                        background: 'rgba(255, 255, 255, 0.2)',
+                        padding: '30px',
+                        marginTop: '10px',
+                        width: '800px',
+                        margin: '0 auto',
+                    }}
+                >
+                    <Input
+                        value={inputValue}
+                        onChange={handleInputChange}
+                        placeholder="Certificate URL"
+                        type="text"
+                        style={{
+                            width: '550px',
+                            fontSize: '20px',
+                            backgroundColor: 'white',
+                            height: '40px',
+                            paddingLeft: '10px',
+                        }}
+                    />
+                    <Button
+                        variant="contained"
+                        onClick={handleClickOpen}
+                        style={{
+                            background: '#26a06e',
+                            color: '#ffffff',
+                            borderRadius: '0',
+                        }}
+                    >
+                        Search
+                    </Button>
+                </Grid>
+                <SimpleDialog
+                    fullWidth={'md'}
+                    maxWidth={'800md'}
+                    open={open}
+                    onClose={handleClose}
+                    certificates={certificates}
                 />
-                <Button variant="contained" onClick={handleClickOpen} style={{ background: '#26a06e', color: '#ffffff', borderRadius: '0' }}>Search</Button>
-            </Grid>
-            <SimpleDialog
-                fullWidth={'md'}
-                maxWidth={'800md'}
-                open={open}
-                onClose={handleClose}
-                certificates={certificates}
-            />
-            <SnackbarProvider maxSnack={5}>
-                <MyApp/>
-            </SnackbarProvider>
-        </Container>
-    );
-}
+            </Container>
+        <SnackbarProvider />
+      </div>
+    )
+  }
 
 export default Search;
