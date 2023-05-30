92a310a02a32112b0fe8b7d2a13e0f67c2b2973e
TranHuyHiep <tranhuyhiep0710@gmail.com>
2023-05-30 21:02:11 +0700

    update logoo

--- src/layouts/SidebarLayout/Sidebar/index.tsx
+++ src/layouts/SidebarLayout/Sidebar/index.tsx
@@ -78,7 +78,7 @@ function Sidebar() {
             background: theme.colors.alpha.trueWhite[10]
           }}
         />
-        {/* <Box p={2}>
+        <Box p={2}>
           <Button
             href="https://bloomui.com"
             target="_blank"
@@ -90,7 +90,7 @@ function Sidebar() {
           >
             Upgrade to PRO
           </Button>
-        </Box> */}
+        </Box>
       </SidebarWrapper>
       <Drawer
         sx={{
