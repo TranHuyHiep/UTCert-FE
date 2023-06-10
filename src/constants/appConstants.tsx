859653f4c13b7cabc135d89b2b83f496d29272ed
Trinh Xuan Bach <86517817+Bachhsunkid@users.noreply.github.com>
2023-06-10 11:13:06 +0700

    update create

--- src/constants/appConstants.tsx
+++ src/constants/appConstants.tsx
@@ -1,3 +1,2 @@
 export const API_URL = 'https://utcerts.somee.com/api/v1';
-export const API_CREATE_URL = 'https://localhost:44325/api/v1';
-
+export const API_CREATE_URL = 'https://utcerts.somee.com/api/v1';
