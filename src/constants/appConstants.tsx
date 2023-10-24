5964d91eae683ae9654da304f4e60af6fd64fb0d
TranHuyHiep <tranhuyhiep0710@gmail.com>
2023-10-24 13:03:53 +0700

    update

--- src/constants/appConstants.tsx
+++ src/constants/appConstants.tsx
@@ -1,2 +1,5 @@
-export const API_URL = 'https://utcerts.somee.com/api/v1';
-export const API_CREATE_URL = 'https://utcerts.somee.com/api/v1';
+// export const API_URL = 'https://utcerts.somee.com/api/v1';
+// export const API_CREATE_URL = 'https://utcerts.somee.com/api/v1';
+
+export const API_URL = 'https://localhost:44325/api/v1';
+export const API_CREATE_URL = 'https://localhost:44325/api/v1';
